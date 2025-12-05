195adf2e09ded107547f8b872267313d7460ab21
Alford Miller <Big_Al_Doogie@Yahoo.Com>
2025-12-05 01:25:50 -0500

    Added basic window with Bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, close_on_esc)
+        .run();
+}
+
+fn close_on_esc(
+    mut commands: Commands,
+    focused_windows: Query<(Entity, &Window)>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    for (window, focus) in focused_windows.iter() {
+        if !focus.focused {
+            continue;
+        }
+
+        if input.just_pressed(KeyCode::Escape) {
+            commands.entity(window).despawn();
+        }
+    }
 }
